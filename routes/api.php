4344b6285466b904f3bec4c8e9179f88793f4888
Raul <raulbsbrz@gmail.com>
2026-04-28 22:33:14 +0200

    Instalación de Laravel Breeze API, configuración de CORS y limpieza de archivos frontend

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SpotifyController;
 
-Route::get('/status', function () {
-    return response()->json([
-        'message' => '¡API conectada con éxito!',
-        'database' => 'Aiven MySQL está vivo',
-        'status' => 'ready'
-    ]);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-Route::get('/search', [SpotifyController::class, 'search']);
